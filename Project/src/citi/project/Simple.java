7e18e6ad8a0a7df5089df2accdf2180bfe40791a
dell <dell@dell-PC>
2020-11-10 20:43:28 +0530

    done by vasanth

---
+++ Project/src/citi/project/Simple.java
@@ -0,0 +1,10 @@
+package citi.project;
+
+public class Simple {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("It is free of cost");
+	}
+
+}
